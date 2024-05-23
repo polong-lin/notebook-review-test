3424f25763e2f7af2e62a91dbb8105f12cf53b36
Polong Lin <polong-lin@users.noreply.github.com>
2024-05-23 13:58:26 +0000

    fixes after local testing

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,12 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("square", help="display a square of a given number")
+parser.add_argument("square", help="display a square of a given number", type=int)
+parser.add_argument("secretnumber", help="a secret number from Cloud Build vars", type=int)
 
+
+args = parser.parse_args()
 answer = args.square**2
 print("Hello World from the hello_world.py file!")
 print(f"the square of {args.square} equals {answer}")
+print(f"The secret number is {args.secretnumber}")

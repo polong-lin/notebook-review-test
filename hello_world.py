c99ace1c8daaa664979f87c996f33e48092c29d2
Polong Lin <polong-lin@users.noreply.github.com>
2024-05-23 13:47:42 +0000

    added args to hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,9 @@
+import argsparse
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("square", help="display a square of a given number")
+
+answer = args.square**2
 print("Hello World from the hello_world.py file!")
+print(f"the square of {args.square} equals {answer}")
